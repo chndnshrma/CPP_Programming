30fa2d91989f59791e2896077e5900ed18993ab2
Chndn <chndnsharma5036@gmail.com>
2026-02-26 10:58:41 +0530

    initial commit 2

--- hello.cpp
+++ hello.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 using namespace std;
 
-// class A{
-//     public:
-//     void show(){
-//         cout<<"Hello \n";
-//     }
-// };
-// class B : public A{
-//     public:
-//     void show(){
-//         cout<<"CSE \n";
-//     }
-// };
-// class C : public B{
-//     public:
-//     void display(){
-//         cout<<"DS \n";
-//     }
-// };
+class A{
+    public:
+    void show(){
+        cout<<"Hello \n";
+    }
+};
+class B : public A{
+    public:
+    void show(){
+        cout<<"CSE \n";
+    }
+};
+class C : public B{
+    public:
+    void display(){
+        cout<<"DS \n";
+    }
+};
 int main(){
-    // C obj;
-    // obj.A::show();
-    // obj.B::show();
-    // obj.display();
+    C obj;
+    obj.A::show();
+    obj.B::show();
+    obj.display();
 
-    int a = 9;
-    int b = 8;
-    cout<<"\n";
-    cout<<a+b;
     return 0;
 }
